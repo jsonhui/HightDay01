24e5a8f59c6c200619e0ea98f42d0794f9b4b441
Administrator <320175912@qq.com>
2016-06-27 16:49:37 +0800

    加了一个注释

--- app/src/main/java/com/yuanke/hightday01/MainActivity.java
+++ app/src/main/java/com/yuanke/hightday01/MainActivity.java
@@ -1,7 +1,15 @@
 package com.yuanke.hightday01;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 /**
  * @Author ：Created by Jason on 2016/6/27 13:53
@@ -9,12 +17,19 @@ import android.os.Bundle;
  * @desc : 这是一个测试
  */
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.home_content_tv)
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //这就是一个注释
-        //又加了一个注释
+        ButterKnife.bind(this);
+        tv.setText("这是黄油刀");
+    }
+
+    @OnClick(R.id.home_submit_btn)
+    public void submit(View view) {
+        Toast.makeText(MainActivity.this, "这是黄油刀监听", Toast.LENGTH_SHORT).show();
     }
 }
