0171bcc5d2f7a44009defed1dc0f628cdc3705e1
Administrator <320175912@qq.com>
2016-06-27 14:02:20 +0800

    加了一个注释

---
+++ app/src/main/java/com/yuanke/hightday01/MainActivity.java
@@ -0,0 +1,19 @@
+package com.yuanke.hightday01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @Author ：Created by Jason on 2016/6/27 13:53
+ * @Email : 320175912@qq.com
+ * @desc : 这是一个测试
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //这就是一个注释
+    }
+}
